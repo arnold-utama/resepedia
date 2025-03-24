b9d4aaa6ef9bdf32e7b2ce14a717f3e2aa5d484d
arnold-utama <arnoldutama3@gmail.com>
2025-03-24 18:29:31 +0700

    feat: Add user, region, and recipe models with migrations

---
+++ server/models/user.js
@@ -0,0 +1,42 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasMany(models.Recipe, { foreignKey: "UserId" });
+    }
+  }
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "Email is required" },
+          notEmpty: { msg: "Email is required" },
+          isEmail: { msg: "Email must be in a valid format" },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "Password is required" },
+          notEmpty: { msg: "Password is required" },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};
