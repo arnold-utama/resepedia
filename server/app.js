6347dc3c86f27417041ff0025fa5dc4ea61b3fcc
arnold-utama <arnoldutama3@gmail.com>
2025-03-25 16:35:50 +0700

    feat: Add CORS support to the Express server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors')
 const router = require("./routes");
 const app = express();
 const port = 3000;
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", router);
