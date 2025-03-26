5b473742aa1e41d83d4189f7bf8499904f45e372
arnold-utama <arnoldutama3@gmail.com>
2025-03-27 03:03:32 +0700

    feat: Set up server structure with initial app configuration and testing setup

--- server/app.js
+++ server/app.js
@@ -1,15 +1,16 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 require("dotenv").config();
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const router = require("./routes");
 const app = express();
-const port = 3000;
 
-app.use(cors())
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", router);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+module.exports = app;
